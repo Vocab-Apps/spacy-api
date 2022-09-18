bfb5258a011c1dfbb66bf5de8351539657b75201
luc wastiaux <luc.wastiaux@airpost.net>
2022-09-18 22:06:13 +0800

    allow testing external urls

--- test_api.py
+++ test_api.py
@@ -5,6 +5,9 @@ import pprint
 import requests
 from api import app
 
+# testing an external URL:
+# SPACY_API_EXTERNAL_URL=http://localhost:8042 pytest test_api.py
+
 class ApiTests(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
@@ -23,26 +26,28 @@ class ApiTests(unittest.TestCase):
 
     def get_query(self, path):
         if self.external_url != None:
-            return requests.get(self.external_url + path)
+            response = requests.get(self.external_url + path)
+            return response.json()
         else:
-            return self.client.get(path)
+            response = self.client.get(path)
+            return json.loads(response.data)
 
     def post_query(self, path, data):
         if self.external_url != None:
-            return requests.post(self.external_url + path, data=data)
+            response = requests.post(self.external_url + path, json=data)
+            return response.json()
         else:
-            return self.client.post(path, json=data)
+            response = self.client.post(path, json=data)
+            return json.loads(response.data)
 
     def test_languages(self):
-        response = self.get_query('/v1/language_list')
-        data = json.loads(response.data)
+        data = self.get_query('/v1/language_list')
         self.assertIn('en', data)
 
     def test_tokenize_english(self):
         text = "I was reading today's paper."
 
-        response = self.post_query('/v1/tokenize', {'language': 'en', 'text': text})
-        data = json.loads(response.data)        
+        data = self.post_query('/v1/tokenize', {'language': 'en', 'text': text})
 
         pprint.pprint(data)
         
@@ -87,8 +92,7 @@ class ApiTests(unittest.TestCase):
     def test_tokenize_french(self):
         text = "Le nouveau plan d’investissement du gouvernement."
 
-        response = self.post_query('/v1/tokenize', data={'language': 'fr', 'text': text})
-        data = json.loads(response.data)        
+        data = self.post_query('/v1/tokenize', data={'language': 'fr', 'text': text})
 
         pprint.pprint(data)
         
@@ -138,8 +142,7 @@ class ApiTests(unittest.TestCase):
     def test_tokenize_chinese_chars(self):
         text = "送外卖的人"
 
-        response = self.post_query('/v1/tokenize', data={'language': 'zh_char', 'text': text})
-        data = json.loads(response.data)        
+        data = self.post_query('/v1/tokenize', data={'language': 'zh_char', 'text': text})
 
         expected_result_chars = [{'can_translate': True,
             'can_transliterate': True,
@@ -168,8 +171,7 @@ class ApiTests(unittest.TestCase):
     def test_tokenize_chinese_words(self):
         text = "送外卖的人"
 
-        response = self.post_query('/v1/tokenize', data={'language': 'zh_jieba', 'text': text})
-        data = json.loads(response.data)        
+        data = self.post_query('/v1/tokenize', data={'language': 'zh_jieba', 'text': text})
 
         expected_result_words = [{'can_translate': True,
             'can_transliterate': True,
