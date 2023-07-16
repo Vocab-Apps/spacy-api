2c5eb413ef6be0a80ce202f04d8f33518fd836a6
Luc W <lucw@airpost.net>
2023-07-16 09:32:45 +0800

    remove comment

--- package.sh
+++ package.sh
@@ -3,7 +3,6 @@
 VERSION_NUMBER=$1 # for example 0.1
 GIT_TAG=v${VERSION_NUMBER}
 
-#echo "CLOUD_LANGUAGE_TOOLS_VERSION='${VERSION_NUMBER}'" > version.py
 git commit -a -m "upgraded version to ${VERSION_NUMBER}"
 git push
 git tag -a ${GIT_TAG} -m "version ${GIT_TAG}"
